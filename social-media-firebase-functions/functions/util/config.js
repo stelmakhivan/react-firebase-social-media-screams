d2e6e3e53b415972b3885c3c5c6569f502b89f5b
Ivan Stelmakh <stelmahivan1994@gmail.com>
2019-09-28 20:26:12 +0300

    refactored code

---
+++ social-media-firebase-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: process.env.API_KEY,
+  authDomain: 'social-media-screams.firebaseapp.com',
+  databaseURL: 'https://social-media-screams.firebaseio.com',
+  projectId: 'social-media-screams',
+  storageBucket: 'social-media-screams.appspot.com',
+  messagingSenderId: '544240547593',
+  appId: '1:544240547593:web:67490fff8c411a766a85d8',
+  measurementId: 'G-JBWNPQBWCN'
+};
