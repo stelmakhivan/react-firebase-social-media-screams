0b5be3d9cfc2c9f83f6c324dbb7df8c5a3c0c2b3
Ivan Stelmakh <stelmahivan1994@gmail.com>
2019-09-28 16:22:46 +0300

    Added Express API to firebase; test routes setuped

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+const db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'First Scream !!!',
+      createdAt: '2019-09-28T13:09:45.853Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
--- social-media-firebase-functions/functions/index.js
+++ social-media-firebase-functions/functions/index.js
@@ -1,8 +1,50 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const express = require('express');
+const app = express();
+
+app.get('/screams', (req, res) => {
+  admin
+    .firestore()
+    .collection('screams')
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then(data => {
+      const screams = [];
+      data.forEach(doc => {
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
+      });
+      return res.json(screams);
+    })
+    .catch(err => console.error(err));
+});
+
+app.post('/scream', (req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString()
+  };
+
+  admin
+    .firestore()
+    .collection('screams')
+    .add(newScream)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'something went wrong' });
+      console.error(err);
+    });
+});
+
+exports.api = functions.region('europe-west1').https.onRequest(app);
