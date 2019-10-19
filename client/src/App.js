0f0ed1dd4a5a306907ea2aa7f8d0f9ece7228f7e
Ivan Stelmakh <stelmakh.developer@gmail.com>
2019-10-19 12:42:33 +0300

    Basic client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Social Media</h1>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route path="/login" component={login} />
+            <Route path="/signup" component={signup} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
