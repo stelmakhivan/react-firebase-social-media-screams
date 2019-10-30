ff0be39027e720ea0bae6e1b4e3d44e6cf4570a2
Ivan Stelmakh <stelmakh.developer@gmail.com>
2019-10-30 21:15:28 +0200

    Refactored theme configuration

--- client/src/App.js
+++ client/src/App.js
@@ -3,60 +3,19 @@ import { hot } from 'react-hot-loader/root';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import themeConfig from './util/theme';
 
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 import Navbar from './components/Navbar';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#33c9dc',
-      main: '#00bcd4',
-      dark: '#008394',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: '#ff6333',
-      main: '#ff3d00',
-      dark: '#b22a00',
-      contrastText: '#fff'
-    }
-  },
-  typography: {
-    useNextVariants: true
-  },
-  form: {
-    form: {
-      margin: 'auto',
-      textAlign: 'center'
-    },
-    image: {
-      maxWidth: 65,
-      maxHeight: 65,
-      margin: '20px auto 20px auto'
-    },
-    pageTitle: {
-      margin: '10px auto 20px auto'
-    },
-    textField: {
-      margin: '10px auto 20px auto'
-    },
-    button: {
-      marginTop: 20,
-      position: 'relative'
-    },
-    customError: {
-      color: 'red',
-      fontSize: '0.8rem',
-      marginTop: 10
-    },
-    progress: {
-      position: 'absolute'
-    }
-  }
-});
+const theme = createMuiTheme(themeConfig);
+
+const token = localStorage.FbIdToken;
+if (token) {
+  // Decode token
+}
 
 function App() {
   return (
---
+++ client/src/util/theme.js
@@ -0,0 +1,48 @@
+export default {
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  },
+  typography: {
+    useNextVariants: true
+  },
+  form: {
+    form: {
+      margin: 'auto',
+      textAlign: 'center'
+    },
+    image: {
+      maxWidth: 65,
+      maxHeight: 65,
+      margin: '20px auto 20px auto'
+    },
+    pageTitle: {
+      margin: '10px auto 20px auto'
+    },
+    textField: {
+      margin: '10px auto 20px auto'
+    },
+    button: {
+      marginTop: 20,
+      position: 'relative'
+    },
+    customError: {
+      color: 'red',
+      fontSize: '0.8rem',
+      marginTop: 10
+    },
+    progress: {
+      position: 'absolute'
+    }
+  }
+};
