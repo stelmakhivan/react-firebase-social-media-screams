500b40e0cfd0c5802d6cf0b5fa8dd7dabc663ef2
Ivan Stelmakh <stelmakh.developer@gmail.com>
2019-11-21 09:07:01 +0200

    Fixed typo in types.js

--- client/src/redux/types.js
+++ client/src/redux/types.js
@@ -11,5 +11,5 @@ export const LOADING_DATA = 'LOADING_DATA';
 // Data reducer types
 export const SET_SCREAMS = 'SET_SCREAMS';
 export const SET_SCREAM = 'SET_SCREAM';
-export const LIKE_SCREAM = 'LIKE_SCREAM';Í
+export const LIKE_SCREAM = 'LIKE_SCREAM';
 export const UNLIKE_SCREAM = 'UNLIKE_SCREAM';
