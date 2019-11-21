a07a305567ffa5284448b2cc0038101b14c979b2
Ivan Stelmakh <stelmakh.developer@gmail.com>
2019-11-21 08:34:13 +0200

    NavBar Buttons

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,28 +1,66 @@
-import React, { Component } from 'react';
+import React, { PureComponent, Fragment } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import theme from '../util/theme'
 
+// MUI components
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 
-export class Navbar extends Component {
+// icons
+import AddIcon from '@material-ui/icons/Add';
+import HomeIcon from '@material-ui/icons/Home';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+import MyButton from '../util/MyButton';
+
+export class Navbar extends PureComponent {
+  static propTypes = {
+    authenticated: PropTypes.bool.isRequired
+  };
+
   render() {
+    const { authenticated } = this.props;
     return (
       <AppBar>
         <Toolbar className="nav-container">
-          <Button color="inherit" component={Link} to="/login">
-            Login
-          </Button>
-          <Button color="inherit" component={Link} to="/">
-            Home
-          </Button>
-          <Button color="inherit" component={Link} to="/signup">
-            Signup
-          </Button>
+          {authenticated ? (
+            <Fragment>
+              <MyButton tip="Post a screen">
+                <AddIcon htmlColor={theme.palette.primary.contrastText} />
+              </MyButton>
+              <Link to="/">
+                <MyButton tip="Home">
+                  <HomeIcon htmlColor={theme.palette.primary.contrastText} />
+                </MyButton>
+              </Link>
+              <MyButton tip="Notifications">
+                <NotificationsIcon htmlColor={theme.palette.primary.contrastText} />
+              </MyButton>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <Button color="inherit" component={Link} to="/login">
+                Login
+              </Button>
+              <Button color="inherit" component={Link} to="/">
+                Home
+              </Button>
+              <Button color="inherit" component={Link} to="/signup">
+                Signup
+              </Button>
+            </Fragment>
+          )}
         </Toolbar>
       </AppBar>
     );
   }
 }
 
-export default Navbar;
+const mapStateToProps = state => ({
+  authenticated: state.user.authenticated
+});
+
+export default connect(mapStateToProps)(Navbar);
