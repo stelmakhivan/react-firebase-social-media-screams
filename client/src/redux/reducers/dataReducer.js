4b4457da028e1bf735455fa5049dcfd8d0f2429b
Ivan Stelmakh <stelmakh.developer@gmail.com>
2019-11-21 09:00:16 +0200

    Added like/unlike and other actions for dataReducer and userReducer

--- client/src/redux/reducers/dataReducer.js
+++ client/src/redux/reducers/dataReducer.js
@@ -1,7 +1,43 @@
-const initialState = {};
+import {
+  SET_SCREAMS,
+  LOADING_DATA,
+  LIKE_SCREAM,
+  UNLIKE_SCREAM
+} from '../types';
+
+const initialState = {
+  screams: [],
+  scream: {},
+  loading: false
+};
 
 export default function dataReducer(state = initialState, action) {
   switch (action.payload) {
+    case LOADING_DATA:
+      return {
+        ...state,
+        loading: true
+      }
+    case SET_SCREAMS:
+      return {
+        ...state,
+        screams: action.payload,
+        loading: false
+      }
+    case LIKE_SCREAM:
+    case UNLIKE_SCREAM:
+      const index = state.screams.findIndex(
+        scream => scream.screamId === action.payload.screamId
+      );
+      const screams = state.screams.reduce((accumulator, prevValue) => {
+        accumulator.push(prevValue);
+        return accumulator
+      }, []);
+      screams[index] = action.payload;
+      return {
+        ...state,
+        screams
+      }
     default:
       return state;
   }
